f7d99fd47c3993334d9ef3ae507eb01621c8c9f3
Sukhdeep Singh Dol <sdol1@studentmail.nait.ca>
2015-09-21 17:07:14 -0600

    Setup of DLL/Component Library with Entity samples

---
+++ eRestaurantDemo/eRestaurantSystem/Entities/SpecialEvent.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+#endregion 
+
+namespace eRestaurantSystem.Entities
+{
+    public class SpecialEvent
+    {
+        [Key]
+        [Required(ErrorMessage="An Event Code is required (only one character)")]
+        [StringLength(1, ErrorMessage="Event Code can only use a single-character code")]
+        public string EventCode { get; set; }
+
+        [Required(ErrorMessage = "A Description is required (5-30 characters)")]
+        [StringLength(30, MinimumLength=5, ErrorMessage="Description must be 5-30 characters in length")]
+        public string Description { get; set; }
+
+        public bool Active { get; set; }
+
+        //Navigation virtual property
+        public virtual ICollection<Reservation> Reservations { get; set; }
+
+        //all classes can have their own constructor
+        //constructor can contain initialization values
+ 
+        public SpecialEvent()
+        {
+            Active = true;
+        }
+
+
+    }
+}
---
+++ eRestaurantDemo/eRestaurantSystem/Entities/Table.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+#endregion 
+
+namespace eRestaurantSystem.Entities
+{
+    public class Table
+    {
+        [Key]
+        public int TableID { get; set; }
+        [Required, Range(1,25)]
+        public byte TableNumber { get; set; }
+        public bool Smoking { get; set; }
+        public int Capacity { get; set; }
+        public bool Available { get; set; }
+        
+        //Navigation Property
+        //the reservations table is a many to many relationship
+        //to tables table
+        //The sql reservations table resolves this problem
+        //However Reservation table holds only a compound primary key
+        //We will not create a Reservation table entity in our project
+        //      but handle it via navigation mapping.
+        //Therefore we will place a ICollection properties in
+        //      this entity refering to the Tables table
+
+        public virtual ICollection<Reservation> Reservations { get; set; }
+
+        public Table()
+        {
+            Available = true;
+        }
+    }
+}
