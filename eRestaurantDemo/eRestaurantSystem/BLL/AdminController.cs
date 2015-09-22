386eec42b4044940520cefca86de0f3b31d36943
Sukhdeep Singh Dol <sdol1@studentmail.nait.ca>
2015-09-22 16:22:03 -0600

    DLL mapping

---
+++ eRestaurantDemo/eRestaurantSystem/BLL/AdminController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+using eRestaurantSystem.Entities;
+using eRestaurantSystem.DAL;
+using System.ComponentModel; // use for ODS access
+#endregion
+
+namespace eRestaurantSystem.BLL
+{
+    [DataObject]
+    class AdminController
+    {
+        [DataObjectMethod(DataObjectMethodType.Select,false)]
+        public List<SpecialEvent> SpecialEvent_List()
+        {
+            using (var context = new eRestaurantContext())
+            {
+                //retreieve the data from the SpecialEvents Table sql
+                //to do so we will use te DbSet in eResturantContext
+                //      call SpecialEvents (done by mapping)
+
+                //method syntax
+                return context.SpecialEvents.OrderBy(x => x.Description).ToList();
+
+            }
+        }
+    }
+}
