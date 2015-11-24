6d3837b65aa8dd5e1f6a6f64912a06b78e1d9bc3
Sukhdeep Singh Dol <sdol1@studentmail.nait.ca>
2015-11-24 16:18:37 -0700

    Setup Connection String and Classes for Security

---
+++ eRestaurantDemo/eRestaurantSystem/BLL/Security/RoleManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using eRestaurantSystem.DAL.Security;
+using eRestaurantSystem.Entities.Security;
+#endregion
+
+namespace eRestaurantSystem.BLL.Security
+{
+    public class RoleManager: RoleManager<IdentityRole>
+    {
+        public RoleManager()
+            :base(new RoleStore<IdentityRole>(new ApplicationDbContext()))
+        {
+
+        }
+
+        public void AddDefaultRoles()
+        {
+            foreach (string roleName in SecurityRoles.DefaultSecurityRoles)
+            {
+                // Check if it exists
+                if (!Roles.Any(r => r.Name == roleName))
+                {
+                    this.Create(new IdentityRole(roleName));
+                }
+            }
+        }
+    }
+}
--- eRestaurantDemo/eRestaurantSystem/Entities/Security/ApplicationUser.cs
+++ eRestaurantDemo/eRestaurantSystem/Entities/Security/ApplicationUser.cs
@@ -13,5 +13,7 @@ namespace eRestaurantSystem.Entities.Security
     // You can add User data for the user by adding more properties to your User class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
+        public int? WaiterID { get; set; }
+
     }
 }
